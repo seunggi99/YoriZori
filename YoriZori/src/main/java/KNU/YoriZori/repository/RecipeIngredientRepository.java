9791ae6f0c13920a24d1360af0764abed692d83e
kimseunggi <ninny9988@naver.com>
2024-03-18 23:32:36 +0900

    add countInsufficientIngredient in RecipeService

---
+++ YoriZori/src/main/java/KNU/YoriZori/repository/RecipeIngredientRepository.java
@@ -0,0 +1,13 @@
+package KNU.YoriZori.repository;
+
+import KNU.YoriZori.domain.PutIn;
+import KNU.YoriZori.domain.RecipeIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+    List<RecipeIngredient> findAllByRecipeId(Long recipeId);
+}
